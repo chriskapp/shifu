2dc8999fb58a4c7059950af289d1e65aa7f64308
Christoph Kappestein <k42b3.x@gmail.com>
2013-08-17 21:32:06 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-shifu
-=====
+Shifu
+========
 
-Solve conversion, arithmetic and logical challenges
+Welcome, shifu is a small application to solve conversion, arithmetic and 
+logical challenges. It is possible to set the difficulty of the challenges and
+exclude specific challange types. All results are stored in an sqlite database.
+
+![Screenshot](http://blog.k42b3.com/img/shifu-screenshot.png)
---
+++ src/com/k42b3/shifu/Entry.java
@@ -0,0 +1,38 @@
+/**
+ * shifu
+ * A application to solve conversion, arithmetic and logical challenges. For the 
+ * current version and more informations visit <https://github.com/k42b3/shifu>
+ * 
+ * Copyright (c) 2013 Christoph Kappestein <k42b3.x@gmail.com>
+ * 
+ * This file is part of Shifu. Shifu is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or at any later version.
+ * 
+ * Shifu is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Shifu. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.k42b3.shifu;
+
+/**
+ * Shifu
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://shifu.k42b3.com
+ */
+public class Entry
+{
+	public static void main(String[] args)
+	{
+		Shifu trainer = new Shifu();
+		trainer.setVisible(true);
+	}
+}
